d7452a42a32d690542d280c2c9db3becbd721e5d
danielpickett <daniel@danielpickett.me>
2021-04-28 11:47:38 -0400

    re-enable maxChroma on slider in Shade component

--- src/ThemeGenerator/components/Shade/Shade.tsx
+++ src/ThemeGenerator/components/Shade/Shade.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import './Shade.scss'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { colorDataPlusSelector, chromaAtom, ShadeType } from 'internal'
-// import { maxChromaSelector } from 'ThemeGenerator/state'
+import { maxChromaSelector } from 'ThemeGenerator/state'
 
 export const Shade = ({ shade }: { shade: ShadeType }) => {
   const [chroma, setChroma] = useRecoilState(chromaAtom(shade))
   const colorData = useRecoilValue(colorDataPlusSelector(shade))
-  // const maxChroma = useRecoilValue(maxChromaSelector(shade))
+  const maxChroma = useRecoilValue(maxChromaSelector(shade))
   // const backgroundColor = colorData.isClipped ? 'black' : colorData.hex
   const backgroundColor = colorData.hex
 
   const handleChromaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = +e.target.value
-    // setChroma(value < maxChroma ? value : maxChroma)
-    setChroma(value)
+    setChroma(value < maxChroma ? value : maxChroma)
+    // setChroma(value)
   }
 
   return (
@@ -29,14 +29,14 @@ export const Shade = ({ shade }: { shade: ShadeType }) => {
       <div>{`${shade.scaleName}-${shade.shadeName}`}</div>
       <div>
         <pre>
-          {`l: ${colorData.lch.l}\n`}
-          {`c: ${colorData.lch.c}\n`}
-          {`h: ${colorData.lch.h}\n`}
+          {`l: ${colorData.lch.l.toFixed(3)}\n`}
+          {`c: ${colorData.lch.c.toFixed(3)}\n`}
+          {`h: ${colorData.lch.h.toFixed(3)}\n`}
           {`${colorData.hex}\n`}
           {`${colorData.rgb.join()}\n`}
-          {`${colorData.contrastOnWhite}`}
+          {`${colorData.contrastOnWhite.toFixed(3)}\n`}
 
-          {/* {`maxChroma: ${maxChroma.toFixed(3)}`} */}
+          {`maxChroma: ${maxChroma.toFixed(3)}`}
           <br />
           <input
             type="range"
