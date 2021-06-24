93da87919341d01823e98137c57d78eb0716c26c
danielpickett <daniel@danielpickett.me>
2021-06-24 14:02:46 -0400

    vivid-subdued added to textColorsState

--- src/ThemeGenerator/state/textColorState.ts
+++ src/ThemeGenerator/state/textColorState.ts
@@ -12,18 +12,32 @@ import {
 import { getMaxChroma } from 'ThemeGenerator/utils'
 
 const regularTextConfig = {
-  '000': { srcShadeName: '900', subduedMix: 0.25 },
-  '050': { srcShadeName: '900', subduedMix: 0.25 },
-  '100': { srcShadeName: '900', subduedMix: 0.25 },
-  '200': { srcShadeName: '900', subduedMix: 0.25 },
-  '300': { srcShadeName: '900', subduedMix: 0.25 },
-  '400': { srcShadeName: '900', subduedMix: 0.25 },
-  '500': { srcShadeName: '000', subduedMix: 0.25 },
-  '600': { srcShadeName: '000', subduedMix: 0.25 },
-  '700': { srcShadeName: '000', subduedMix: 0.25 },
-  '800': { srcShadeName: '000', subduedMix: 0.25 },
-  '900': { srcShadeName: '000', subduedMix: 0.25 },
+  '000': { srcShadeName: '900', mix: 0.25 },
+  '050': { srcShadeName: '900', mix: 0.25 },
+  '100': { srcShadeName: '900', mix: 0.25 },
+  '200': { srcShadeName: '900', mix: 0.25 },
+  '300': { srcShadeName: '900', mix: 0.25 },
+  '400': { srcShadeName: '900', mix: 0.25 },
+  '500': { srcShadeName: '000', mix: 0.25 },
+  '600': { srcShadeName: '000', mix: 0.25 },
+  '700': { srcShadeName: '000', mix: 0.25 },
+  '800': { srcShadeName: '000', mix: 0.25 },
+  '900': { srcShadeName: '000', mix: 0.25 },
 } as const
+
+export const safeLums = {
+  '000': { lum: 45, operator: '<' },
+  '050': { lum: 39, operator: '<' },
+  '100': { lum: 35, operator: '<' },
+  '200': { lum: 32, operator: '<' },
+  '300': { lum: 30, operator: '<' },
+  '400': { lum: 18.2, operator: '>' },
+  '500': { lum: 100, operator: '>' },
+  '600': { lum: 95, operator: '>' },
+  '700': { lum: 90, operator: '>' },
+  '800': { lum: 85, operator: '>' },
+  '900': { lum: 20, operator: '>' },
+}
 
 export const vividTextColorConfig = {
   '000': { lum: 45, maxLum: 50, mix: 0.25 },
@@ -36,13 +50,14 @@ export const vividTextColorConfig = {
   '600': { lum: 95, minLum: 100, mix: 0.25 },
   '700': { lum: 90, minLum: 71, mix: 0.25 },
   '800': { lum: 85, minLum: 59.1, mix: 0.25 },
-  '900': { lum: 80, minLum: 52.5, mix: 0.25 },
+  '900': { lum: 20, minLum: 52.5, mix: 0.25 },
 } as const
 
 type TextColorsType = {
   regular: ColorDataType
   subdued: ColorDataType
   vivid: ColorDataType
+  'vivid-subdued': ColorDataType
 }
 
 export const textColorsSelector = selectorFamily<TextColorsType, ShadeType>({
@@ -69,18 +84,24 @@ export const textColorsSelector = selectorFamily<TextColorsType, ShadeType>({
       const subduedText = mix(
         regularText.hex,
         swatch.hex,
-        regularTextConfig[shadeName].subduedMix
+        regularTextConfig[shadeName].mix
       )
 
-      const lum = vividTextColorConfig[shadeName].lum
+      const lum = safeLums[shadeName].lum
       const chroma = getMaxChroma(lum, hue)
 
       const vividText = getColorData(lum, chroma, hue)
+      const vividSubduedText = mix(
+        vividText.hex,
+        swatch.hex,
+        vividTextColorConfig[shadeName].mix
+      )
 
       return {
         regular: regularText,
         subdued: subduedText,
         vivid: vividText,
+        'vivid-subdued': vividSubduedText,
       }
     },
 })
