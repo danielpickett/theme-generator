7421acc397f796528907b9c615dc72994d08f40c
danielpickett <daniel@danielpickett.me>
2021-06-24 13:58:18 -0400

    Header and settingsState

--- src/ThemeGenerator/components/index.ts
+++ src/ThemeGenerator/components/index.ts
@@ -1,6 +1,7 @@
 export * from './Canvas'
 export * from './CanvasPointsOverlay'
 export * from './ChromaSlider'
+export * from './Header'
 export * from './HueSlider'
 export * from './RawCSSVarsOutput'
 export * from './SampleText'
--- src/ThemeGenerator/state/index.ts
+++ src/ThemeGenerator/state/index.ts
@@ -1,3 +1,4 @@
-export * from './swatchColorState'
 export * from './scaleNamesState'
+export * from './settingsState'
+export * from './swatchColorState'
 export * from './textColorState'
