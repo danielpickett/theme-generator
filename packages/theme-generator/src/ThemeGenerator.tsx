3b3079ea14adf9dc0a3ba1bf1c29892293407232
Daniel Pickett <daniel.pickett@availity.com>
2022-09-04 12:54:33 -0400

    fix typescript issue in parcel config

--- packages/theme-generator/src/ThemeGenerator.tsx
+++ packages/theme-generator/src/ThemeGenerator.tsx
@@ -4,29 +4,25 @@ import { RecoilRoot } from 'recoil'
 import { Footer, Header, Scale } from './components'
 import { DEFAULT_THEME_SCALE_NAMES } from './themes'
 
-export const ThemeGenerator = () => (
-  <RecoilRoot>
-    <ThemeGeneratorBase />
-  </RecoilRoot>
-)
-
-const ThemeGeneratorBase = () => {
+export const ThemeGenerator = () => {
   return (
-    <div className="ThemeGenerator dark-blue-theme">
-      <div className="ThemeGenerator__header">
-        <Header />
-      </div>
-      <div className="ThemeGenerator__body">
-        <div className="ThemeGenerator__scales">
-          {DEFAULT_THEME_SCALE_NAMES.map((scaleName) => (
-            <Scale key={scaleName} scaleName={scaleName} />
-          ))}
+    <RecoilRoot>
+      <div className="ThemeGenerator dark-blue-theme">
+        <div className="ThemeGenerator__header">
+          <Header />
+        </div>
+        <div className="ThemeGenerator__body">
+          <div className="ThemeGenerator__scales">
+            {DEFAULT_THEME_SCALE_NAMES.map((scaleName) => (
+              <Scale key={scaleName} scaleName={scaleName} />
+            ))}
+          </div>
+        </div>
+
+        <div className="ThemeGenerator__footer">
+          <Footer />
         </div>
       </div>
-
-      <div className="ThemeGenerator__footer">
-        <Footer />
-      </div>
-    </div>
+    </RecoilRoot>
   )
 }
